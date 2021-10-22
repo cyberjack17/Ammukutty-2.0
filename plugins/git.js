2d06bab9f7b768f7fa2f5f1db41ee6102bd7dd4d
ᴄʏʙᴇʀ㊌ᴊᴀᴄᴋ <92788624+cyberjack17@users.noreply.github.com>
2021-10-22 13:46:38 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,22 +7,20 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("Ammukutty-2.0.png", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Ammukutty-2.0 bot created by ᴄʏʙᴇʀ㊌ᴊᴀᴄᴋ*
 *Creator number : wa.me/917994922550?text=Hi%20Cyber%20Jack.%20*
 
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*Githublink (Setup)  :    https://github.com/cyberjack17/Ammukutty-2.0*
 
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Audio commads :   https://github.com/cyberjack17/Ammukutty-2.0/tree/master/uploads*
 
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Sticker commads : https://github.com/cyberjack17/Ammukutty-2.0/tree/master/uploads*
+ 
 
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
-
-*For More Updates Subscribe The Channel ☝*
 `}) 
 
 }));
